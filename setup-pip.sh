a94bd2b85c7937d5d6fdd5d53fa02d369b4aeeba
DMC <nkhess@umich.edu>
2025-09-09 14:30:32 -0400

    update setup

--- setup-pip.sh
+++ setup-pip.sh
@@ -2,6 +2,6 @@ echo -e "\e[32mStarting setup for slack-Lambda-button for Linux...\e[39m"
 
 # install requirements
 sudo apt-get install -y python3-dev libasound2-dev
-pip install --upgrade -r requirements.txt
+pip install --upgrade --break-system-packages -r requirements.txt
 
 echo -e "\e[32mAll done! You may now run slack-Lambda-button via \"python gui.py\"\e[39m"
