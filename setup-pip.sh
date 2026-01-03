5299b6d6b82cd54e8c6411dffe737b92826264b2
Nikki <coinslotgaming@gmail.com>
2026-01-02 19:27:59 -0500

    :fire: remove unnecessary setup files

--- setup-pip.sh
+++
@@ -1,7 +0,0 @@
-echo -e "\e[32mStarting setup for slack-Lambda-button for Linux...\e[39m"
-
-# install requirements
-sudo apt-get install -y python3-dev libasound2-dev
-pip install --upgrade --break-system-packages -r requirements.txt
-
-echo -e "\e[32mAll done! You may now run slack-Lambda-button via \"python gui.py\"\e[39m"
--- setup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting setup for slack-Lambda-button for Windows...
-
-pip install --upgrade pip setuptools wheel
-pip install --upgrade -r requirements.txt
-
-echo All done! You may now run slack-Lambda-button via "python gui.py"
-pause
